5f8b27002f03c61a0e95a0557f5572d635d4337a
subham22choudhary <subham22choudhary@gmail.com>
2024-04-15 03:12:14 +0530

    dip-1

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+
+import logo from '../assets/investment-calculator-logo.png';
+
+export default function Header() {
+    return (
+        <>
+            <header id="header">
+                <img src={logo} alt="Money Bag" />
+                <h1>Investment Calculator</h1>
+            </header>
+        </>
+
+    )
+
+
+}
